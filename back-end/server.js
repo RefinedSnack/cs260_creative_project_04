1f9b11ebb3e3312378152e2f661e58e45b866e62
Kylie Callaway <majorkylie404@gmail.com>
2021-12-04 12:46:46 -0700

    I added a rating line and style for the comments section

--- back-end/server.js
+++ back-end/server.js
@@ -4,81 +4,84 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 
 const mongoose = require('mongoose');
 
 // connect to the database
 mongoose.connect('mongodb://localhost:27017/gamegenius', {
-    useNewUrlParser: true
+  useNewUrlParser: true
 });
 
 // Create a scheme for comments
 const commentSchema = new mongoose.Schema({
-    content: String,
-    time: Number,
-    author: String,
-    page: String
+  content: String,
+  time: Number,
+  author: String,
+  page: String,
+  rating: String
 });
 
 // Create a model for comments
 const Comment = mongoose.model('Comments', commentSchema);
 
 //upload a comment
-app.post('/api/comment/:id', async(req, res) => {
-    const comment = new Comment({
-        content: req.body.content,
-        time: req.body.time,
-        author: req.body.author,
-        page: req.params.id
-    });
-    try {
-        await comment.save();
-        res.send(comment);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
+app.post('/api/comment/:id', async (req, res) => {
+  const comment = new Comment({
+    content: req.body.content,
+    time: req.body.time,
+    author: req.body.author,
+    page: req.params.id,
+    rating: req.body.rating
+  });
+  try {
+    await comment.save();
+    res.send(comment);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 //read comments by page
-app.get('/api/comment/:id', async(req, res) => {
-    try {
-        const comments = await Comment.find({
-            page: req.params.id
-        });
-        res.send(comments);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
+app.get('/api/comment/:id', async (req, res) => {
+  try {
+    const comments = await Comment.find({
+      page: req.params.id
+    });
+    res.send(comments);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 //updated a comment
-app.put('/api/comment/:id', async(req, res) => {
-    try {
-        const comment = await Comment.findByIdAndUpdate(req.params.id, {
-            content: req.body.content,
-            time: req.body.time,
-            author: req.body.author
-        });
-        res.send(comment);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
+app.put('/api/comment/:id', async (req, res) => {
+  try {
+    const comment = await Comment.findByIdAndUpdate(req.params.id, {
+      content: req.body.content,
+      time: req.body.time,
+      author: req.body.author,
+      rating: req.body.rating
+    });
+    res.send(comment);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 //deletes a comment based on the comment _id
-app.delete('/api/comment/:id', async(req, res) => {
-    try {
-        await Comment.findByIdAndRemove(req.params.id);
-        res.sendStatus(200);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
+app.delete('/api/comment/:id', async (req, res) => {
+  try {
+    await Comment.findByIdAndRemove(req.params.id);
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 
--- front-end/src/components/CommentForm.vue
+++ front-end/src/components/CommentForm.vue
@@ -1,27 +1,32 @@
 <template>
 <div class="comment-form">
-    <form>
-        <div class="form-group">
-            <label for="username">Username: </label>
-            <input type="text" class="form-control" id="username" v-model="username">
-            <p></p>
-            <label for="content">Comment: </label>
-            <textarea class="form-control" rows="5" id="content" v-model="content">
+  <form>
+    <div class="form-group">
+      <label for="username">Username: </label>
+      <input type="text" class="form-control" id="username" v-model="username">
+      <p></p>
+      <label for="rating">Rating (1-10): </label>
+      <input type="text" class="form-control" id="rating" v-model="rating">
+      <p></p>
+      <label for="content">Comment: </label>
+      <textarea class="form-control" rows="5" id="content" v-model="content">
             </textarea>
-        </div>
-        <button class="delete-button" v-on:click="onSubmit()">Submit</button>
-    </form>
-
-        <div class="Comment" v-for="(comment, index) in comments" :key="index">
-            <div class="comment-data">
-                <p>{{ comment.content }}</p>
-                <p>Posted by {{ comment.author }} @ {{ time(comment.time) }}</p>
-            </div>
-            <div class="comment-control">
-                <button class="delete-button" v-on:click="deleteComment(comment._id)">Delete</button>
-                <button class="replace-button" v-on:click="editComment(comment._id)">Replace</button>
-            </div>
-        </div>
+    </div>
+    <button class="delete-button" v-on:click="onSubmit()">Submit</button>
+  </form>
+  <div class="Comments">
+  <div class="Comment" v-for="(comment, index) in comments" :key="index">
+    <div class="comment-data">
+      <p id="review">{{ comment.content }}</p>
+      <p id="rating">Rating: {{ comment.rating }}</p>
+      <p id="author">Posted by {{ comment.author }} @ {{ time(comment.time) }}</p>
+    </div>
+    <div class="comment-control">
+      <button class="delete-button" v-on:click="deleteComment(comment._id)">Delete</button>
+      <button class="replace-button" v-on:click="editComment(comment._id)">Replace</button>
+    </div>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -29,68 +34,88 @@
 import axios from 'axios';
 
 export default {
-    name: 'CommentForm',
-    components: {},
-    data() {
-        return {
-            content: '',
-            username: '',
-            comments: [],
-        }
-    },
-    props: {
-        formID: String
-    },
-    created() {
-        this.getComments();
-    },
-    methods: {
-        //get comment async sets loating to true
-        getComments() {
-            axios.get('/api/comment/' + this.formID)
-                .then(response => {
-                    this.comments = response.data;
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-        },
-        onSubmit() {
-            try {
-                console.log(this.formID, this.author, this.content);
-                let postString = 'api/comment/' + this.formID;
-                axios.post(postString, {
-                    content: this.content,
-                    time: Date.now(),
-                    author: this.username
-                });
-
-            } catch (error) {
-                console.log(error);
-            }
-            this.getComments();
-        },
-        deleteComment(commentID) {
-            axios.delete('/api/comment/' + commentID);
-            this.getComments();
-        },
-        editComment(commentID) {
-            axios.put('/api/comment/' + commentID, {
-                content: this.content,
-                author: this.username,
-                time: Date.now()
-            });
-            this.getComments();
-        },
-        time(input) {
-            var date = new Date(input);
-            let time = date.toLocaleString();
-            return time;
-        },
+  name: 'CommentForm',
+  components: {
+    //  StarRating
+  },
+  data() {
+    return {
+      content: '',
+      username: '',
+      comments: [],
+      rating: ''
     }
+  },
+  props: {
+    formID: String
+  },
+  created() {
+    this.getComments();
+  },
+  methods: {
+    //get comment async sets loating to true
+    getComments() {
+      axios.get('/api/comment/' + this.formID)
+        .then(response => {
+          this.comments = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    },
+    onSubmit() {
+      try {
+        console.log(this.formID, this.author, this.content);
+        let postString = 'api/comment/' + this.formID;
+        axios.post(postString, {
+          content: this.content,
+          time: Date.now(),
+          author: this.username,
+          rating: this.rating
+        });
+
+      } catch (error) {
+        console.log(error);
+      }
+      this.getComments();
+    },
+    deleteComment(commentID) {
+      axios.delete('/api/comment/' + commentID);
+      this.getComments();
+    },
+    editComment(commentID) {
+      axios.put('/api/comment/' + commentID, {
+        content: this.content,
+        author: this.username,
+        time: Date.now(),
+        rating: this.rating
+      });
+      this.getComments();
+    },
+    time(input) {
+      var date = new Date(input);
+      let time = date.toLocaleString();
+      return time;
+    },
+  }
 }
 </script>
 
 <style>
+.Comment {
+  background-color: #D3FAD7;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+  padding: 20px 20px 20px 20px;
+  font-style: italic;
+  color: #22577A;
+}
+
+.Comments  {
+margin-top: 50px;
+margin-bottom: 50px;
+}
 
 </style>
